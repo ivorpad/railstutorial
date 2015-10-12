ca2beb2720e350e3a1f07835d417d45b615b01bc
ivorpad <ivorjpc@gmail.com>
2015-10-12 07:57:13 -0430

    Added the edit form and its link in the dropdown menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  root                  'static_pages#home'
-  get     'help'    =>  'static_pages#help'
-  get     'about'   =>  'static_pages#about'
-  get     'contact' =>  'static_pages#contact'
-  get     'signup'  =>  'users#new'
-  get     'login'   =>  'sessions#new'
-  post    'login'   =>  'sessions#create'
-  delete  'logout'  =>  'sessions#destroy'
+  root                           'static_pages#home'
+  get     'help'           =>    'static_pages#help'
+  get     'about'          =>    'static_pages#about'
+  get     'contact'        =>    'static_pages#contact'
+  get     'signup'         =>    'users#new'
+  get     'user/:id/edit'  =>    'users#edit'
+  get     'login'          =>    'sessions#new'
+  post    'login'          =>    'sessions#create'
+  delete  'logout'         =>    'sessions#destroy'
 
   resources :users
 end
